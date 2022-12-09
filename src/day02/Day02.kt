4f60fae9001d9196d1554d182d75a393b8e34b85
Josh Stallnick <violabs.software@gmail.com>
2022-12-09 13:48:23 -0600

    Day 2

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -1,13 +1,14 @@
-package day01
+package day02
 
 import readInput
 
+
 fun main() {
-    test("day-01-test-input-01", 24000)
+    test("day-02-test-input-01", 45000)
 }
 
 private fun test(filename: String, expected: Int) {
-    val input = readInput("day01/$filename")
+    val input = readInput("day02/$filename")
 
     val actual: Int = findPositionOfElfWithMostCalories(input)
 
@@ -31,26 +32,28 @@ private fun findPositionOfElfWithMostCalories(input: List<String>): Int {
         tracker.addCaloriesToSum(calories)
     }
 
-    return tracker.highestCalories
+    return tracker
+        .finalCalories()
+        .sortedDescending()
+        .take(3)
+        .sum()
 }
 
 class ElfCalorieTracker(
     var currentSum: Int = 0,
-    var highestCalories: Int = 0,
+    var caloriesList: MutableList<Int> = mutableListOf()
 ) {
 
 
     fun moveToNextElf() {
-        if (currentSum > highestCalories) updateHighestCalories()
+        caloriesList.add(currentSum)
 
         currentSum = 0
-    }
-
-    private fun updateHighestCalories() {
-        highestCalories = currentSum
     }
 
     fun addCaloriesToSum(calories: String) {
         currentSum += calories.toInt()
     }
+
+    fun finalCalories(): MutableList<Int> = caloriesList.also { it.add(currentSum) }
 }
