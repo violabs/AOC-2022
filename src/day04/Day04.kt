be3d6bb2aef1ebd44bbd8e62d3194c608a5b3cc1
Josh Stallnick <violabs.software@gmail.com>
2022-12-09 16:22:11 -0600

    Day 4 Part 2

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -5,6 +5,8 @@ import readInput
 
 fun main() {
     test1("day-04-test-input-01", 2)
+    test2("day-04-test-input-01", 4)
+    test2("day-04-actual-test-input", 4)
 }
 
 private fun test1(filename: String, expected: Int) {
@@ -18,11 +20,30 @@ private fun test1(filename: String, expected: Int) {
     check(expected == actual)
 }
 
+private fun test2(filename: String, expected: Int) {
+    val input = readInput("day04/$filename")
+
+    val actual: Int = findAnyOverlapInCleaningAssignments(input)
+
+    println("EXPECT: $expected")
+    println("ACTUAL: $actual")
+
+    check(expected == actual)
+}
+
 private fun findHighPriorityCleaningReassignments(input: List<String>): Int {
     return input
         .asSequence()
         .map(::WorkPair)
         .filter(WorkPair::containsHighPriorityReassignment)
+        .count()
+}
+
+private fun findAnyOverlapInCleaningAssignments(input: List<String>): Int {
+    return input
+        .asSequence()
+        .map(::WorkPair)
+        .filter(WorkPair::containsAnyOverlap)
         .count()
 }
 
@@ -40,6 +61,13 @@ class WorkPair(details: String) {
 
     fun containsHighPriorityReassignment(): Boolean =
         firstSections.contains(secondSections) || secondSections.contains(firstSections)
+
+    fun containsAnyOverlap(): Boolean =
+        firstSections.sharesAny(secondSections) || secondSections.sharesAny(firstSections)
 }
 
 fun Pair<Int, Int>.contains(other: Pair<Int, Int>): Boolean = this.first <= other.first && this.second >= other.second
+
+fun Pair<Int, Int>.sharesAny(other: Pair<Int, Int>): Boolean =
+    (this.first <= other.first && this.second >= other.first) ||
+    (this.first <= other.second && this.second >= other.second)
